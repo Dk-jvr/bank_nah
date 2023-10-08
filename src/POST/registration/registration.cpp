d4926c62c5f170079ff118425a9bf40e82efe9a4
dk-jvr <dimaskivas42@gmail.com>
2023-10-08 12:54:29 +0300

    registration final

--- src/POST/registration/registration.cpp
+++ src/POST/registration/registration.cpp
@@ -10,12 +10,17 @@
 #include <userver/formats/json/value.hpp>
 #include <userver/crypto/hash.hpp>
 #include <userver/formats/json/value_builder.hpp>
+#include <userver/server/http/http_response.hpp>
+#include <userver/server/http/http_status.hpp>
+#include <userver/utils/uuid4.hpp>
 
 namespace pg = userver::storages::postgres;
 
 namespace pg_service_template {
 
     namespace {
+
+        const std::string kInsertUser = R"~(INSERT INTO hello_schema.users VALUES ($1, bytea($2), uuid($3)))~";
 
         class Registration final : public userver::server::handlers::HttpHandlerJsonBase {
             
@@ -29,17 +34,26 @@ namespace pg_service_template {
             userver::formats::json::Value HandleRequestJsonThrow(const userver::server::http::HttpRequest& request, 
                 const userver::formats::json::Value &request_json, userver::server::request::RequestContext &context) const override {
                 
-                std::string email = request_json["email"].As<std::string>();
-                std::string password_str = request_json["password"].As<std::string>();
-                std::string password = userver::crypto::hash::Sha256(password_str, userver::crypto::hash::OutputEncoding::kHex);
-                auto result = pg_cluster_->Execute(pg::ClusterHostType::kMaster, "INSERT INTO hello_schema.users "
-                    "VALUES ($1, bytea($2))", email, password);
-
+                userver::server::http::HttpResponse& response = request.GetHttpResponse();
                 userver::formats::json::ValueBuilder builder;
-                builder["email"] = email;
-                builder["password"] = password;
-
-                return builder.ExtractValue();
+                try
+                {
+                    std::string login = request_json["login"].As<std::string>();
+                    std::string password_str = request_json["password"].As<std::string>();
+                    std::string password = userver::crypto::hash::Sha256(password_str, userver::crypto::hash::OutputEncoding::kHex);
+                    std::string user_id = userver::utils::generators::GenerateUuid();
+                    auto result = pg_cluster_->Execute(pg::ClusterHostType::kMaster, kInsertUser, login, password, user_id);
+                    builder["login"] = login;
+                    builder["password"] = password;
+                    builder["user_id"] = user_id;
+                    return builder.ExtractValue();
+                }
+                catch(const std::exception& ex)
+                {
+                    response.SetStatus(userver::server::http::HttpStatus::kBadRequest);
+                    LOG_ERROR() << "Exception due to: " << ex.what();
+                    return builder.ExtractValue();
+                }
             }
             pg::ClusterPtr pg_cluster_;
         };
